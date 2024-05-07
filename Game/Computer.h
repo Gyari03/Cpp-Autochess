4fcea33213c60568e69f130ac2f1fe98f71ea56f
Gabor <gy.gabor03@gmail.com>
2024-05-07 18:10:32 +0200

    Minor changes

--- Game/Computer.h
+++ Game/Computer.h
@@ -10,7 +10,7 @@ public:
     Computer():white(nullptr),black(nullptr){}
     Computer(Team* white,Team* black):white(white),black(black){}
     void calculateMoves(TeamColor color){
-        //FILLE FÜGGVÉNYEK CSAK HOGY LEFORDULJON
+        //FILLE FÜGGVÉNYEK CSAK HOGY LEFORDULJON //
         if(color==White){
             white->getArmy();
         }
